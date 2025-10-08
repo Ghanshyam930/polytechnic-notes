759d757969c8de23b1e42522f1921115d877c5bf
Ghanshyam <ghanshyamj9755@gmail.com>
2025-10-08 20:21:48 +0530

    Update settings.py

--- notes/settings.py
+++ notes/settings.py
@@ -107,7 +107,7 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUD_NAME'),
     'API_KEY': os.environ.get('API_KEY'),
     'API_SECRET': os.environ.get('API_SECRET'),
-    'SECURE': True,
+    # 'SECURE': True,
 }
 
 
